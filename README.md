198cf03fc59ea9496cc2dda5646aca1b67013eaf
Nick <tishlakov.nn@gmail.com>
2026-07-05 07:41:08 +0300

    mid1_1

--- README.md
+++ README.md
@@ -1,11 +1,26 @@
 # Go Learning Roadmap
 
 Current level: Middle 1
-Next task: mid1_1
+Next task: mid1_2
 
 Weak Areas:
 -
 
 ## Completed
 
+- [x] mid1_1 Manual Errgroup Fundamentals
+  - fail-fast coordination
+  - error propagation
+  - context cancellation
+  - WaitGroup synchronization
+  - orchestrator pattern
+  - cooperative goroutine shutdown
+
 ## Topics learned
+
+- Fail-fast pattern
+- Manual error propagation
+- Orchestrator responsibility
+- Group lifecycle coordination
+- WaitGroup + Context composition
+- Why errgroup exists
--- mid/mid1_1/main.go
+++ mid/mid1_1/main.go
@@ -1,5 +1,42 @@
 package main
 
-func main() {
+import (
+	"context"
+	"errors"
+	"fmt"
+	"sync"
+	"time"
+)
 
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	wg := sync.WaitGroup{}
+	errorChan := make(chan error, 1)
+
+	wg.Go(func() {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println("worker 1 OK")
+	})
+	wg.Go(func() {
+		time.Sleep(150 * time.Millisecond)
+		errorChan <- errors.New("worker 2 down")
+	})
+	wg.Go(func() {
+		timer := time.NewTimer(300 * time.Millisecond)
+
+		select {
+		case <-timer.C:
+			fmt.Println("worker 3 OK")
+		case <-ctx.Done():
+			timer.Stop()
+			fmt.Println("worker 3 Context OFF")
+			return
+		}
+	})
+
+	err := <-errorChan
+	cancel()
+	fmt.Println("---> ERROR: ", err)
+
+	wg.Wait()
 }
