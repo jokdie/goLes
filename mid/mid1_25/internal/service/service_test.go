b8beb1826c891a906c0fb3a0215a6a14860ee83c
Nick <tishlakov.nn@gmail.com>
2026-07-20 07:17:55 +0300

    test update

--- mid/mid1_25/internal/service/service_test.go
+++ mid/mid1_25/internal/service/service_test.go
@@ -7,10 +7,17 @@ import (
 )
 
 type fakeRepository struct {
+	ctx     context.Context
+	id      int
+	called  bool
 	getFunc func(ctx context.Context, id int) error
 }
 
 func (f *fakeRepository) Get(ctx context.Context, id int) error {
+	f.ctx = ctx
+	f.called = true
+	f.id = id
+
 	return f.getFunc(ctx, id)
 }
 
@@ -29,18 +36,47 @@ func TestUserService_GetUser_Success(t *testing.T) {
 
 	service := NewUserService(userRep, orderRep, profileRep)
 
-	err := service.GetUser(t.Context(), 123)
+	id := 123
+
+	err := service.GetUser(t.Context(), id)
 	if err != nil {
 		t.Fatal(err)
+	}
+	if !userRep.called {
+		t.Fatal("userRep does not used")
+	}
+	if !orderRep.called {
+		t.Fatal("orderRep does not used")
+	}
+	if !profileRep.called {
+		t.Fatal("profileRep does not used")
+	}
+	if userRep.id != id {
+		t.Fatalf("userRep != %d", id)
+	}
+	if orderRep.id != id {
+		t.Fatalf("orderRep != %d", id)
+	}
+	if profileRep.id != id {
+		t.Fatalf("profileRep != %d", id)
+	}
+	if userRep.ctx == nil {
+		t.Fatal("userRep - context was not passed")
+	}
+	if orderRep.ctx == nil {
+		t.Fatal("orderRep - context was not passed")
+	}
+	if profileRep.ctx == nil {
+		t.Fatal("profileRep - context was not passed")
 	}
 }
 
 func TestUserService_GetUser_UserRepositoryError(t *testing.T) {
-	UserError := errors.New("db error")
+	userError := errors.New("db error")
 
 	userRep := &fakeRepository{
 		getFunc: func(ctx context.Context, id int) error {
-			return UserError
+			return userError
 		},
 	}
 	orderRep := getRepWithoutError()
@@ -48,46 +84,76 @@ func TestUserService_GetUser_UserRepositoryError(t *testing.T) {
 
 	service := NewUserService(userRep, orderRep, profileRep)
 
-	err := service.GetUser(t.Context(), 123)
-	if !errors.Is(err, UserError) {
-		t.Fatalf("expected %v, got %v", UserError, err)
+	id := 123
+	err := service.GetUser(t.Context(), id)
+	if !errors.Is(err, userError) {
+		t.Fatalf("expected %v, got %v", userError, err)
+	}
+	if !userRep.called {
+		t.Fatal("user repository was not called")
+	}
+	if userRep.id != id {
+		t.Fatalf("expected id %d, got %d", id, userRep.id)
+	}
+	if userRep.ctx == nil {
+		t.Fatal("userRep - context was not passed")
 	}
 }
 
 func TestUserService_GetUser_OrderRepositoryError(t *testing.T) {
-	OrderError := errors.New("db error")
+	orderError := errors.New("db error")
 
 	userRep := getRepWithoutError()
 	orderRep := &fakeRepository{
 		getFunc: func(ctx context.Context, id int) error {
-			return OrderError
+			return orderError
 		},
 	}
 	profileRep := getRepWithoutError()
 
 	service := NewUserService(userRep, orderRep, profileRep)
 
-	err := service.GetUser(t.Context(), 123)
-	if !errors.Is(err, OrderError) {
-		t.Fatalf("expected %v, got %v", OrderError, err)
+	id := 123
+	err := service.GetUser(t.Context(), id)
+	if !errors.Is(err, orderError) {
+		t.Fatalf("expected %v, got %v", orderError, err)
+	}
+	if !orderRep.called {
+		t.Fatal("order repository was not called")
+	}
+	if orderRep.id != id {
+		t.Fatalf("expected id %d, got %d", id, orderRep.id)
+	}
+	if orderRep.ctx == nil {
+		t.Fatal("orderRep - context was not passed")
 	}
 }
 
 func TestUserService_GetUser_ProfileRepositoryError(t *testing.T) {
-	ProfileError := errors.New("db error")
+	profileError := errors.New("db error")
 
 	userRep := getRepWithoutError()
 	orderRep := getRepWithoutError()
 	profileRep := &fakeRepository{
 		getFunc: func(ctx context.Context, id int) error {
-			return ProfileError
+			return profileError
 		},
 	}
 
 	service := NewUserService(userRep, orderRep, profileRep)
 
-	err := service.GetUser(t.Context(), 123)
-	if !errors.Is(err, ProfileError) {
-		t.Fatalf("expected %v, got %v", ProfileError, err)
+	id := 123
+	err := service.GetUser(t.Context(), id)
+	if !errors.Is(err, profileError) {
+		t.Fatalf("expected %v, got %v", profileError, err)
+	}
+	if !profileRep.called {
+		t.Fatal("profile repository was not called")
+	}
+	if profileRep.id != id {
+		t.Fatalf("expected id %d, got %d", id, profileRep.id)
+	}
+	if profileRep.ctx == nil {
+		t.Fatal("profileRep - context was not passed")
 	}
 }
